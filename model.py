f42dc9e675bd18a49aec663b1d758a6ab125da4d
aylinayt <aylin.s.aytemiz@wmich.edu>
2024-04-05 10:12:27 -0400

    eren's code

--- model.py
+++ model.py
@@ -63,6 +63,7 @@ for i, ax in enumerate(axes.flatten()):
     class_name = class_labels[label]  # Get the class label name
 
     #ax.imshow(image, cmap='gray')
+    ax.imshow(image, cmap='gray')
     ax.set_title(f"Label: {class_name}")
     ax.axis('off')
 
@@ -74,6 +75,149 @@ num_epochs = 25
 learning_rate = 0.001
 num_classes = len(class_labels)
 
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5)
+        self.conv4 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5)
+
+        self.fc1 = nn.Linear(in_features=64*16, out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=64)
+        self.fc3 = nn.Linear(in_features=64, out_features=num_classes)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+
+        x = x.view(x.size(0), -1) # Flatten
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
+    
+
+model = ConvNet().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+def train_model(model, criterion, optimizer, train_loader, num_epochs):
+    total_step = len(train_loader)
+
+    for epoch in range(num_epochs):
+        start_time = time.time()
+        running_loss = 0.0
+
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            # Print training progress
+            if (i+1) % 20 == 0:
+                elapsed_time = time.time() - start_time
+                print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_step}], Loss: {loss.item():.4f}, Elapsed Time: {elapsed_time:.2f} sec')
+                start_time = time.time()
+
+        # Calculate average loss for the epoch
+        epoch_loss = running_loss / len(train_loader)
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}')
+
+# Merge train and test datasets
+train_dataset = image_datasets['train']
+test_dataset = image_datasets['test']
+
+merged_dataset = torch.utils.data.ConcatDataset([train_dataset, test_dataset])
+
+# Create a new combined DataLoader
+merged_dataloader = torch.utils.data.DataLoader(merged_dataset, batch_size=6, shuffle=True, num_workers=0)
+
+train_model(model, criterion, optimizer, merged_dataloader, num_epochs)
+
+# Evaluate the model
+def evaluate_model(model, dataloader):
+    model.eval()
+    correct = 0
+    total = 0
+    y_true = []
+    y_pred = []
+
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+            y_true.extend(labels.cpu().numpy())
+            y_pred.extend(predicted.cpu().numpy())
+
+    accuracy = correct / total
+
+    # Calculate F1 score, precision, and recall for each class
+    f1_scores = f1_score(y_true, y_pred, average=None)
+    precision_scores = precision_score(y_true, y_pred, average=None)
+    recall_scores = recall_score(y_true, y_pred, average=None)
+
+    return accuracy, f1_scores, precision_scores, recall_scores
+
+# Evaluate the model on the validation set
+val_loader = dataloaders['valid']
+accuracy, f1_scores, precision_scores, recall_scores = evaluate_model(model, val_loader)
+
+print(f"Overall Model Accuracy: {accuracy}\n")
+# Print the results
+for i in range(num_classes):
+    class_label = class_labels[i]
+    print(f"Class: {class_label}")
+    print(f"F1 score: {f1_scores[i]}")
+    print(f"Precision: {precision_scores[i]}")
+    print(f"Recall: {recall_scores[i]}")
+    print()
+
+
+
+# Save model
+FILE = 'ct-scan-model.pth'
+torch.save(model.state_dict(), FILE)
+
+
+# Manuel test
+# Load and preprocess the test image
+test_image = Image.open('./testdata.jpg').convert('L')
+test_image = composed_transform(test_image)
+test_image = test_image.unsqueeze(0).to(device)
+
+# Pass the test image through the model for prediction
+model.eval()
+with torch.no_grad():
+    output = model(test_image)
+    _, predicted_class = torch.max(output, 1)
+
+# Get the predicted class label
+predicted_label = class_labels[predicted_class.item()]
+print("Predicted Label:", predicted_label)
 
 # create the model still working on this part
 
